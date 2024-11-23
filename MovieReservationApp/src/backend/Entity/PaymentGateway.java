ca1ffa2cd6f60530af4f7ba9833dcf37c1ba92a8
aryan <aryanmogaveera@gmail.com>
2024-11-23 02:18:08 -0700

    add payement gateway

--- MovieReservationApp/src/backend/Entity/PaymentGateway.java
+++ MovieReservationApp/src/backend/Entity/PaymentGateway.java
@@ -1,5 +1,37 @@
 package backend.Entity;
 
+import java.time.LocalDateTime;
+
 public class PaymentGateway {
-    
+    private String transactionId;
+    private String status;
+    private double amount;
+    private LocalDateTime timestamp;
+
+
+    //Constructor
+    public PaymentGateway (String transactionId, String status, double amount, LocalDateTime timestamp) {
+        this.transactionId = transactionId;
+        this.status = status;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+    // Getters and Setters
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
