e4daec0a30e0bf328c4ba0752e3dbc6ecce4b876
Sheharyar. <sheharyar@ucalgary.ca>
2024-11-25 11:42:55 -0700

    adminstaff class [not final]

--- MovieReservationApp/src/backend/actors/AdminStaff.java
+++ MovieReservationApp/src/backend/actors/AdminStaff.java
@@ -1,5 +1,16 @@
 package backend.actors;
 
 public class AdminStaff {
+    private final String username;
+    private final String password;
     
+    public AdminStaff(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public boolean authenticate(String attemptedUsername, String attemptedPassword) {
+        return this.username.equals(attemptedUsername) && 
+               this.password.equals(attemptedPassword);
+    }
 }
